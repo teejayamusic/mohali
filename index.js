630c35285758678316798acab5df7e861d2ff3c9
teejayamusic <142833479+teejayamusic@users.noreply.github.com>
2024-08-29 22:26:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ db.connect((err) => {
 // Multer setup for image uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, 'uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
